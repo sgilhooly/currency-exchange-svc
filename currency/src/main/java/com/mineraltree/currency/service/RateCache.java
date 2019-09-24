9dae964ab953103a13199f7f747eb55d7e3eec4f
Kathan Master <Kathan.Master@mineraltree.com>
2019-09-24 14:35:32 -0400

    Refactored to prevent multiple requests from hitting loader

--- currency/src/main/java/com/mineraltree/currency/service/RateCache.java
+++ currency/src/main/java/com/mineraltree/currency/service/RateCache.java
@@ -3,16 +3,17 @@ package com.mineraltree.currency.service;
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.Props;
-import akka.actor.Status;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
 import akka.japi.pf.ReceiveBuilder;
 import com.mineraltree.currency.ControlCode;
 import com.mineraltree.currency.GetRatesRequest;
-import com.mineraltree.currency.ServiceNotReady;
+import com.mineraltree.currency.RatesUnavailable;
 import com.mineraltree.currency.dto.CurrencyRates;
 import java.time.Duration;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 /**
@@ -26,6 +27,7 @@ public class RateCache extends AbstractActor {
   private ActorRef rateSource;
   private final Props rateSourceProps;
   private final Duration refreshInterval;
+  private final Set<String> inFlight = new HashSet<>();
 
   public static Props mkProps(Props rateSourceProps, Duration refreshInterval) {
     return Props.create(RateCache.class, rateSourceProps, refreshInterval);
@@ -62,20 +64,33 @@ public class RateCache extends AbstractActor {
         .match(CurrencyRates.class, this::updateCurrentRates)
         .match(ControlCode.class, this::handleControl)
         .match(GetRatesRequest.class, this::retrieveRates)
+        .match(String.class, this::retryRetrieveRates)
         .build();
   }
 
   private void retrieveRates(GetRatesRequest request) {
+    if (!"[A-Za-z]{3}".matches(request.getBase())) {
+      getSender()
+          .tell(
+              new RatesUnavailable(
+                  new RuntimeException("Currency must be formatted as a 3 letter string")),
+              getSelf());
+    }
+
     boolean isBaseLoaded = currentRates.containsKey(request.getBase());
     if (request.responseExpected() && isBaseLoaded) {
       getSender().tell(currentRates.get(request.getBase()), getSelf());
     } else {
-      if (request.responseExpected()) {
-        getSender().tell(new Status.Failure(new ServiceNotReady()), getSelf());
-      }
-      // TODO: don't let bursts of requests get through to the loader (only load once)
       log.info("[base={}] First request for currency rates. Fetching now.", request.getBase());
-      rateSource.tell(request, getSelf());
+
+      if (!inFlight.contains(request.getBase())) {
+        rateSource.tell(request, getSelf());
+        inFlight.add(request.getBase());
+      }
+
+      if (request.responseExpected()) {
+        scheduleRetryRetrieveRates(request.getBase());
+      }
     }
   }
 
@@ -85,6 +100,7 @@ public class RateCache extends AbstractActor {
         rates.getBaseCurrency(),
         rates.getProvider());
     currentRates.put(rates.getBaseCurrency(), rates);
+    inFlight.remove(rates.getBaseCurrency());
   }
 
   private void handleControl(ControlCode code) {
@@ -92,6 +108,22 @@ public class RateCache extends AbstractActor {
       rateSource.tell(ControlCode.REFRESH, getSelf());
     } else {
       unhandled(code);
+    }
+  }
+
+  private void scheduleRetryRetrieveRates(String base) {
+    getContext()
+        .system()
+        .scheduler()
+        .scheduleOnce(
+            Duration.ofMillis(100), getSelf(), base, getContext().dispatcher(), getSender());
+  }
+
+  private void retryRetrieveRates(String base) {
+    if (currentRates.containsKey(base)) {
+      getSender().tell(currentRates.get(base), getSelf());
+    } else {
+      scheduleRetryRetrieveRates(base);
     }
   }
 }
